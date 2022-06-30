0b00c3f87f1c924fa60003bf9399bb6d6c7ea259
SkyderG <44149740+SkyderG@users.noreply.github.com>
2022-06-30 18:51:10 -0300

    new subcommands introduced

--- src/lottery/command/BaseCommand.php
+++ src/lottery/command/BaseCommand.php
@@ -25,10 +25,10 @@ class BaseCommand extends Command
      */
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool
     {
-        if(!$sender instanceof Player) return false;
+        if (!$sender instanceof Player) return false;
 
-        if(!isset($args[0])) {
-            if($sender->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
+        if (!isset($args[0])) {
+            if ($sender->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
                 $sender->sendMessage(MessageTranslator::translateNested("usage.admin"));
             } else {
                 $sender->sendMessage(MessageTranslator::translateNested("usage.player"));
@@ -39,17 +39,51 @@ class BaseCommand extends Command
 
         switch ($args[0]) {
             case "bet":
-                if(!isset($args[1])) return false;
+            case "buy":
+                if (!isset($args[1])) {
+                    $sender->sendMessage(MessageTranslator::translateNested("error.no-tip-value"));
+                    return false;
+                }
 
                 $this->plugin->getPluginData()->addPlayer($sender, $args[1]);
                 break;
 
+            case "activate":
+            case "start":
+                if (!$sender->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
+                    $sender->sendMessage(MessageTranslator::translateNested("error.no-permission"));
+                } else {
+                    $this->plugin->getPluginData()->forceStart();
+                    $sender->sendMessage(MessageTranslator::translateNested("success.admin-activate"));
+                }
+                break;
+
             case "cancel":
-                if(!$sender->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
+            case "end":
+                if (!$sender->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
                     $sender->sendMessage(MessageTranslator::translateNested("error.no-permission"));
                 } else {
                     $this->plugin->getPluginData()->refundAll();
+                    $sender->sendMessage(MessageTranslator::translateNested("success.admin-cancel"));
                 }
+                break;
+
+            case "time":
+            case "status":
+                if (!$this->plugin->getPluginData()->isEnabled()) {
+                    $sender->sendMessage(MessageTranslator::translateNested("error.no-activated"));
+                    return false;
+                }
+
+                $data = $this->plugin->getPluginData();
+
+                $message = MessageTranslator::translateNested("announce.running", [
+                    $data->getTotalPrize(),
+                    $data->getPlayerCount(),
+                    gmdate("H:i:s", $data->getTime()),
+                ]);
+
+                $sender->sendMessage($message);
                 break;
         }
 
